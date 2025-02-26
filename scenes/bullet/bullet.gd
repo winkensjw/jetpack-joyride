f391a23a2668f11522beb74bb39a3376658e0bf8
winkensjw <84124931+winkensjw@users.noreply.github.com>
2025-02-26 15:50:59 +0100

    Add bullets to bullet group and remove them when off screen

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -6,3 +6,7 @@ const SPEED = 1000.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x += SPEED * delta
+
+
+func _on_visible_checker_screen_exited() -> void:
+		queue_free()
