d7d1802cfabb7aac6e1706e7767f3b4b4a4ccb28
winkensjw <84124931+winkensjw@users.noreply.github.com>
2025-02-27 13:24:55 +0100

    Fix alert sound

--- scenes/alert/alert.gd
+++ scenes/alert/alert.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 @onready var sprite = $Sprite2D
+@onready var alertSound = $AlertSound
 
 func _ready() -> void:
+	if Globals.sound_effects_enabled:
+		alertSound.play()
+		
 	var tween = get_tree().create_tween()
 	tween.set_loops(3)
 	tween.tween_interval(0.1)
